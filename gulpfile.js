4528bfba941bf2e19cf8581199daf0f3541b6980
criErb <cristiano.erbacci@tangible.is>
2020-04-28 17:29:55 +0200

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('html', function() {
+  return gulp.src('dev/*.html')
+  .pipe(gulp.dest('prod/'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('sass', function() {
+    return gulp.src('dev/scss/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('prod/css/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('script', function() {
+    return gulp.src('dev/js/*.js')
+    .pipe(gulp.dest('prod/js/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('images', function() {
+    return gulp.src('dev/img/*')
+    .pipe(gulp.dest('prod/img/'));
+});
+
+// Static server
+gulp.task('serve', function() {
+    browserSync.init({
+        server:  "./prod"
+    });
+
+    gulp.watch('dev/scss/*.scss', gulp.series('sass'));
+    gulp.watch('dev/*.html', gulp.series('html'));
+    gulp.watch('dev/js/*.js', gulp.series('script'));
+});
+
+gulp.task('default', gulp.series(['html','sass','images','serve','script']));
