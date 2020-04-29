2ca476a4cb35e0fc1f0385dc256f02dc6e661ac0
criErb <cristiano.erbacci@tangible.is>
2020-04-29 07:38:40 +0200

    Adds menu js logic and component

---
+++ dev/js/menu.js
@@ -0,0 +1,21 @@
+
+function menuToggle() {
+    var menuPanel = document.getElementById("menu-panel");
+    var menuIconFirst = document.querySelector(".c-menu__toggle span:first-child");
+    var menuIconSecond = document.querySelector(".c-menu__toggle span:nth-child(2)");
+    var menuIconThird = document.querySelector(".c-menu__toggle span:nth-child(3)");
+
+    if (menuPanel.style.display === "none") {
+    menuPanel.style.display = "block";
+    menuIconFirst.style.transform = "rotate(45deg)";
+    menuIconSecond.style.display = "none";
+    menuIconThird.style.transform = "rotate(-45deg)";
+    menuIconThird.style.top = "0";
+} else {
+    menuPanel.style.display = "none";
+    menuIconFirst.style.transform = "rotate(0)";
+    menuIconSecond.style.display = "block";
+    menuIconThird.style.transform = "rotate(0)";
+    menuIconThird.style.top = "16px";
+    }
+}
